44b6cdd8f3707aea08a9d27a91aae0e11612150a
Prudhvi3753 <prudhvich45@gmail.com>
2025-01-18 21:42:31 +0530

    progress

--- Loops/fibonacci.py
+++ Loops/fibonacci.py
@@ -1,4 +1,4 @@
-a=int(input("enter a number:"))
+'''a=int(input("enter a number:"))
 b=0
 c=1
 print(f"{b}\n{c}")
@@ -6,4 +6,11 @@ for i in range(1,a):
     temp=b+c
     b=c
     c=temp
-    print(temp)
+    print(temp)'''
+
+n=int(input("Enter a number:"))
+a,b=0,1
+while(a<=n):
+    print(a,end=" ")
+    a,b=b,a+b
+
